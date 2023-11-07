cb071dfc80d3410038b2e5f32d4dd5dcbced7988
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-08 03:28:01 +0600

    Remove frozen string literal comment

--- person.rb
+++ person.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # the class of Person
 class Person
   attr_accessor :name, :age
--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './person'
 
 # Teacher class a child of Person class
