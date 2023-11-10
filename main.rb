e1cd03800b34592413efa9627c0cfca78d6ff7a4
Al-Amin <alaminkhanshakil@gmail.com>
2023-11-10 19:46:35 +0600

    Add main interface for book list

---
+++ main.rb
@@ -0,0 +1,20 @@
+require './app'
+
+def main
+    app = App.new
+    puts 'Welcome to The School Library'
+    puts ' '
+    loop do
+        puts 'Please select an option by entering a number:'
+        puts '1. List all books'
+        option = get.chomp.to_i
+        case option
+        when 1
+            app.book_list
+        else
+            'Invalid option, try again'
+        end
+    end
+end
+
+main
